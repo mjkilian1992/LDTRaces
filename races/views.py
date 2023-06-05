58747cc7de3c6efb4fccd2229bebcabfbc77aee8
Michael Kilian <michael@allplants.com>
2023-06-05 20:49:57 +0100

    Add cursor pagination for infinite scrolling

--- races/views.py
+++ races/views.py
@@ -1,9 +1,15 @@
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.pagination import CursorPagination
 from django_filters.rest_framework import DjangoFilterBackend
 
 
 from races import models as races_models
 from races import serializers as races_serializers
+
+
+class TicketsPagination(CursorPagination):
+    page_size = 5
+    ordering = '-created_at'
 
 
 class ListCreateTickets(ListCreateAPIView):
@@ -13,3 +19,4 @@ class ListCreateTickets(ListCreateAPIView):
     filterset_fields = (
         'event_id', 'race_id', 'ticket_id'
     )
+    pagination_class = TicketsPagination

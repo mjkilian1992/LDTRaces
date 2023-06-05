6ae162c97399d3189e56b72d65c197c2024fc933
Michael Kilian <michael@allplants.com>
2023-06-05 19:15:04 +0100

    Add basic model for a ticket

--- races/models.py
+++ races/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Ticket(models.Model):
+    """
+    Basic model for an individuals ticket to a race
+    """
+    eventID = models.CharField(max_length=8)
+    raceID = models.CharField(max_length=8)
+    ticketID = models.CharField(max_length=8)
+    eventTitle = models.TextField()
+    raceTitle = models.TextField()
+    ticketTitle = models.TextField()
+    firstName = models.CharField(max_length=32)
+    lastName = models.CharField(max_length=32)
+    email = models.EmailField()

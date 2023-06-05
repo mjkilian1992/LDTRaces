54365f205a01d60a338e9fb3853db0456fc0328c
Michael Kilian <michael@allplants.com>
2023-06-05 20:35:22 +0100

    Add basic filtering on id fields

--- races/views.py
+++ races/views.py
@@ -1,4 +1,6 @@
 from rest_framework.generics import ListCreateAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 from races import models as races_models
 from races import serializers as races_serializers
@@ -7,3 +9,7 @@ from races import serializers as races_serializers
 class ListCreateTickets(ListCreateAPIView):
     queryset = races_models.Ticket.objects.all()
     serializer_class = races_serializers.TicketSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = (
+        'event_id', 'race_id', 'ticket_id'
+    )
